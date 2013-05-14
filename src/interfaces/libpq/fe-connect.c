c6ea6ec7fc0ddc13f22b2b9dcbd0bf64d9ebe7b8
Daniel Farina <daniel@fdr.io>
2013-05-14 15:05:47 -0700

    resolv
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -15,8 +15,10 @@
 
 #include "postgres_fe.h"
 
+#include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <ctype.h>
 #include <time.h>
@@ -379,6 +381,7 @@ static char *PasswordFromFile(char *hostname, char *port, char *dbname,
 static bool getPgPassFilename(char *pgpassfile);
 static void dot_pg_pass_warning(PGconn *conn);
 static void default_threadlock(int acquire);
+static char *executeResolveCmd(const char *conninfo, char *resolveCmd);
 
 
 /* global variable because fe-auth.c needs to access it */
@@ -510,12 +513,162 @@ PQpingParams(const char *const * keywords,
 PGconn *
 PQconnectdb(const char *conninfo)
 {
-	PGconn	   *conn = PQconnectStart(conninfo);
+	PGconn *conn;
+
+	conn = PQconnectStart(conninfo);
 
 	if (conn && conn->status != CONNECTION_BAD)
 		(void) connectDBComplete(conn);
 
 	return conn;
+}
+
+static char *
+executeResolveCmd(const char *conninfo, char *resolveCmd)
+{
+	int saved_errno;
+	pid_t forkStatus = -1;
+
+	size_t conninfoLen = strlen(conninfo);
+
+	int resolveInputPipe[2];
+	int resolveOutputPipe[2];
+
+	char *buf = NULL;
+	size_t bufCap;
+	size_t bufLen;
+
+	size_t readStatus;
+	size_t remain;
+
+	/* Save errno for restoration */
+	saved_errno = errno;
+	errno = 0;
+
+	/*
+	 * Create input/output pipes to get full duplex communication with
+	 * the resolve commmand.
+	 */
+    if (pipe(resolveInputPipe))
+		goto err;
+
+    if (pipe(resolveOutputPipe))
+		goto err;
+
+	forkStatus = fork();
+
+	switch (forkStatus)
+	{
+		case 0:
+			/*
+			 * Child process: shuffle file descriptors around and close
+			 * irrelevant parts of the pipe.
+			 */
+			close(resolveInputPipe[1]);
+			dup2(resolveInputPipe[0], 0);
+
+			close(resolveOutputPipe[0]);
+			dup2(resolveOutputPipe[1], 1);
+
+			execl("/bin/sh", "sh", "-c", resolveCmd, NULL);
+
+			/* If exec didn't work, just get out of the child */
+			exit(1);
+			break;
+
+		case -1:
+			/* Fork is no good: complain */
+			goto err;
+
+		default:
+			/*
+			 * Parent process
+			 *
+			 * Close off the child-owned parts of the pipe.
+			 */
+			close(resolveInputPipe[0]);
+			close(resolveOutputPipe[1]);
+	}
+
+	/*
+	 * Write the entire conninfo passed by the user, or complain and
+	 * die.
+	 *
+	 //Note that this can deadlock for very large conninfo, but thanks
+	 //torelatively generous PIPE_BUF constants as seen on most
+	 //platforms, this is okay for a prototype.
+	 *
+	 */
+	do
+	{
+		errno = 0;
+		write(resolveInputPipe[1], conninfo, conninfoLen);
+	} while (errno == EINTR);
+
+	if (errno)
+		goto err;
+
+	/*
+	 * Close the input part of the pipe: recovery commands rely on
+	 * this to know when the input is finished.
+	 */
+	do
+	{
+		errno = 0;
+
+		close(resolveInputPipe[1]);
+	} while (errno == EINTR);
+
+	if (errno)
+		goto err;
+		
+	/* Buffer the output of the command for return. */
+	buf = pg_malloc(PIPE_BUF);
+	bufCap = PIPE_BUF;
+	bufLen = 0;
+
+	do
+	{
+		remain = bufCap - bufLen;
+
+		/* Allocate slack if short on remaining space. */
+		if (remain < PIPE_BUF)
+		{
+			buf = pg_realloc(buf, bufCap + PIPE_BUF);
+			remain += PIPE_BUF;
+		}
+
+		/* Read from the pipe, tracking progress in 'buf', or die */
+		do
+		{
+			errno = 0;
+
+			readStatus = read(resolveOutputPipe[0], buf + bufLen, remain);
+
+			bufLen += readStatus;
+		} while (errno == EINTR);
+
+		if (errno)
+			goto err;
+
+	} while (readStatus == remain); /* EOF condition */
+
+	buf[bufLen] = '\0';
+
+	return buf;
+
+err:
+	fprintf(stderr, "dies: %d\n", errno);
+
+	if (forkStatus > 0)
+		waitpid(forkStatus, NULL, 0);
+
+	if (buf != NULL)
+		pg_free(buf);
+
+	errno = saved_errno;
+
+	return NULL;
 }
 
 /*
@@ -4048,12 +4201,23 @@ static PQconninfoOption *
 parse_connection_string(const char *connstr, PQExpBuffer errorMessage,
 						bool use_defaults)
 {
+	char *resolveCmd;
+	char *resolved;
+
+	resolveCmd = getenv("PGRESOLVECMD");
+
+	if (resolveCmd != NULL)
+		resolved = executeResolveCmd(connstr, resolveCmd);
+
+	if (resolved == NULL)
+		resolved = (char *) connstr;
+
 	/* Parse as URI if connection string matches URI prefix */
 	if (uri_prefix_length(connstr) != 0)
-		return conninfo_uri_parse(connstr, errorMessage, use_defaults);
+		return conninfo_uri_parse(resolved, errorMessage, use_defaults);
 
 	/* Parse as default otherwise */
-	return conninfo_parse(connstr, errorMessage, use_defaults);
+	return conninfo_parse(resolved, errorMessage, use_defaults);
 }
 
 /*
